f3cb46f35289c7c9711d54567128eb6b266ea545
unknown <alk.pikus@gmail.com>
2021-01-08 17:30:02 +0100

    test: created test for CgooseResourceDialog and refactored component

--- starWars/src/components/ChooseResourceDialog.tsx
+++ starWars/src/components/ChooseResourceDialog.tsx
@@ -5,8 +5,6 @@ import {
   Paragraph,
   Portal,
   RadioButton,
-  Surface,
-  Text,
 } from 'react-native-paper';
 import {useDispatch} from 'react-redux';
 
@@ -17,12 +15,12 @@ const ChooseResourceDialog = (props: {
   isVisible: boolean;
   hideDialog: () => void;
 }) => {
-  const [checked, setChecked] = useState(resource.people);
+  const [value, setValue] = useState<string>(resource.people);
 
   const dispatch = useDispatch();
-  const chooseResource = (val: resource) => {
+  const chooseResource = (val: string) => {
     dispatch(resourceSlice.actions.setResource({resourceValue: val}));
-    setChecked(val);
+    setValue(val);
   };
 
   return (
@@ -32,25 +30,23 @@ const ChooseResourceDialog = (props: {
           <Paragraph>
             To start the game choose which force you want to fight
           </Paragraph>
-          <Surface style={{flexDirection: 'row', alignItems: 'center'}}>
-            <RadioButton
+          <RadioButton.Group
+            onValueChange={(value) => chooseResource(value)}
+            value={value}>
+            <RadioButton.Item
+              accessibilityLabel="People"
+              label="People"
               value={resource.people}
-              status={checked === resource.people ? 'checked' : 'unchecked'}
-              onPress={() => chooseResource(resource.people)}
             />
-            <Text>People</Text>
-          </Surface>
-          <Surface style={{flexDirection: 'row', alignItems: 'center'}}>
-            <RadioButton
+            <RadioButton.Item
+              accessibilityLabel="Starships"
+              label="Starships"
               value={resource.starships}
-              status={checked === resource.starships ? 'checked' : 'unchecked'}
-              onPress={() => chooseResource(resource.starships)}
             />
-            <Text>Starships</Text>
-          </Surface>
+          </RadioButton.Group>
         </Dialog.Content>
         <Dialog.Actions>
-          <Button onPress={props.hideDialog}>Done</Button>
+          <Button onPress={props.hideDialog}>OK</Button>
         </Dialog.Actions>
       </Dialog>
     </Portal>
---
+++ starWars/tests/ChooseResourceDialog.spec.tsx
@@ -0,0 +1,45 @@
+import {configureStore} from '@reduxjs/toolkit';
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import React from 'react';
+
+import ChooseResourceDialog from '../src/components/ChooseResourceDialog';
+import reducers from '../src/reducers';
+import MockProviders from './MockProviders';
+
+describe('<ChooseResourceDialog/>', () => {
+  it('radio should change value on chosen option', async () => {
+    const store = configureStore({reducer: reducers});
+    const hideDialog = jest.fn();
+    const {getByLabelText, queryByText} = render(
+      <MockProviders store={store}>
+        <ChooseResourceDialog isVisible={true} hideDialog={hideDialog} />
+      </MockProviders>,
+    );
+    await waitFor(() => {
+      const radioStarships = getByLabelText('Starships');
+      const radioPeople = getByLabelText('People');
+      expect(radioPeople).toBeChecked;
+      expect(radioStarships).not.toBeChecked;
+      expect(queryByText('current value=people/')).toBeTruthy;
+      fireEvent.press(radioStarships);
+      expect(radioPeople).not.toBeChecked;
+      expect(radioStarships).toBeChecked;
+      expect(queryByText('current value=starships/')).toBeTruthy;
+    });
+  });
+  it('should close and navigate to Game screen after clicking button OK', async () => {
+    const store = configureStore({reducer: reducers});
+    const hideDialog = jest.fn();
+    const {getByText} = render(
+      <MockProviders store={store}>
+        <ChooseResourceDialog isVisible={true} hideDialog={hideDialog} />
+      </MockProviders>,
+    );
+    await waitFor(() => {
+      const okButton = getByText('OK');
+      expect(okButton).toBeTruthy();
+      fireEvent.press(okButton);
+      expect(hideDialog).toHaveBeenCalled();
+    });
+  });
+});
